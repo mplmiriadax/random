03f968c7488ee5761f769b71a887b1cc1c78c0c1
Miguel Pineda <miguel25mayo@gmail.com>
2015-07-13 20:24:29 +0200

    primer commit

---
+++ random.js
@@ -0,0 +1,8 @@
+// Math.random() devuelve número aleatorio entre 0 y 1
+var numero = Math.random();
+
+if (numero <= 0.5){
+  console.log('\n' + numero + ' menor o igual que 0,5 \n');
+}else{
+  console.log('\n' + numero + ' mayor que 0,5 \n');
+}
