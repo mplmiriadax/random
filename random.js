29b2b33854ac9f2de7e8529a039a4c4fb31df367
Miguel Pineda <miguel25mayo@gmail.com>
2015-07-13 20:30:47 +0200

    prueba version 2

--- random.js
+++ random.js
@@ -1,8 +1,10 @@
 // Math.random() devuelve número aleatorio entre 0 y 1
 var numero = Math.random();
 
-if (numero <= 0.5){
-  console.log('\n' + numero + ' menor o igual que 0,5 \n');
+if (numero < 0.5){
+  console.log('\n' + numero + ' menor que 0,5 \n');
+}else if (numero = 0.5){
+  console.log('\n' + numero + ' igual a 0,5 \n');
 }else{
   console.log('\n' + numero + ' mayor que 0,5 \n');
 }
